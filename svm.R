e06204628f5f46b2804a7982914f1461dc55cc84
Deddy Romnan <romnandeddy@gmail.com>
2021-12-07 12:04:48 +0700

    feat: add svm

---
+++ svm.R
@@ -0,0 +1,17 @@
+library(e1071)
+library(caret)
+library(devtools)
+
+msrm <- read.csv(file = "data/mushroom_factored.csv", stringsAsFactors = TRUE)
+
+msrm.train.idx <- sample(x = round(nrow(msrm)), size = round(nrow(msrm)*0.5))
+msrm.train <- msrm[msrm.train.idx,]
+msrm.test <- msrm[-msrm.train.idx,]
+write.csv(x = msrm.train, file = "data/mushroom_train.csv", quote = FALSE, row.names = FALSE)
+write.csv(x = msrm.test, file = "data/mushroom_test.csv", quote = FALSE, row.names = FALSE)
+
+msrm.svm <- svm(class ~ ., data = msrm.train)
+pred <- predict(msrm.svm, msrm.test)
+confusionMatrix(pred, msrm.test$class)
+
+saveRDS(msrm.svm, "model/svm.RDS")
