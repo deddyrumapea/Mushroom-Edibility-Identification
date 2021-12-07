28769e442bb6a864fe7f578f0f5185692ba88d47
Deddy Romnan <romnandeddy@gmail.com>
2021-12-07 12:05:10 +0700

    feat: create app

--- app.R
+++ app.R
@@ -1,49 +1,136 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(data.table)
+library(e1071)
 
-# Define UI for application that draws a histogram
+msrm <- read.csv(file = "data/mushroom_factored.csv", stringsAsFactors = TRUE)
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Mushroom Edibility Identification"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "odor", label = "Odor", choices = levels(msrm$odor)),
+      selectInput(inputId = "habitat", label = "Habitat", choices = levels(msrm$habitat)),
+      selectInput(inputId = "population", label = "Population", choices = levels(msrm$population)),
+      selectInput(inputId = "bruises", label = "Bruises", choices = levels(msrm$bruises)),
+      selectInput(inputId = "veilColor", label = "Veil color", choices = levels(msrm$veil.color)),
+      selectInput(inputId = "sporePrintColor", label = "Spore print color", choices = levels(msrm$spore.print.color)),
+      
+      selectInput(inputId = "ringNumber", label = "Number of rings", choices = levels(msrm$ring.number)),
+      selectInput(inputId = "ringType", label = "Ring type", choices = levels(msrm$ring.type)), 
+      
+      selectInput(inputId = "stalkShape", label = "stalkShape", choices = levels(msrm$stalk.shape)), 
+      selectInput(inputId = "stalkRoot", label = "stalkRoot", choices = levels(msrm$stalk.root)), 
+      selectInput(inputId = "stalkSurfaceAboveRing", label = "stalkSurfaceAboveRing", choices = levels(msrm$stalk.surface.above.ring)), 
+      selectInput(inputId = "stalkSurfaceBelowRing", label = "stalkSurfaceBelowRing", choices = levels(msrm$stalk.surface.below.ring)), 
+      selectInput(inputId = "stalkColorAboveRing", label = "stalkColorAboveRing", choices = levels(msrm$stalk.color.above.ring)), 
+      selectInput(inputId = "stalkColorBelowRing", label = "stalkColorBelowRing", choices = levels(msrm$stalk.color.below.ring)), 
+      
+      selectInput(inputId = "capShape", label = "capShape", choices = levels(msrm$cap.shape)), 
+      selectInput(inputId = "capSurface", label = "capSurface", choices = levels(msrm$cap.surface)), 
+      selectInput(inputId = "capColor", label = "capColor", choices = levels(msrm$cap.color)), 
+      
+      selectInput(inputId = "gillAttachment", label = "gillAttachment", choices = levels(msrm$gill.attachment)), 
+      selectInput(inputId = "gillSpacing", label = "gillSpacing", choices = levels(msrm$gill.spacing)), 
+      selectInput(inputId = "gillSize", label = "gillSize", choices = levels(msrm$gill.size)), 
+      selectInput(inputId = "gillColor", label = "gillColor", choices = levels(msrm$gill.color)),
+      
+      actionButton(inputId = "btnSubmit", label = "Submit", class = "btn btn-primary")
+    ),
+    
+    mainPanel(
+      tags$label(h3('Output')),
+      verbatimTextOutput('contents'),
+      verbatimTextOutput('textResult'),
+      verbatimTextOutput("txtout")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+server <- function(input, output, session) {
+  datasetInput <- function(){
+    df <- data.frame(
+      Name = c("cap.shape", "cap.surface", "cap.color", "bruises", 
+               "odor", "gill.attachment", "gill.spacing", "gill.size", 
+               "gill.color", "stalk.shape", "stalk.root", 
+               "stalk.surface.above.ring", "stalk.surface.below.ring", 
+               "stalk.color.above.ring", "stalk.color.below.ring", "veil.color", 
+               "ring.number", "ring.type", "spore.print.color", "population", 
+               "habitat"),
+      Value = as.character(c(
+        input$capShape, input$capSurface, input$capColor, input$bruises, 
+        input$odor, input$gillAttachment, input$gillSpacing, input$gillSize, 
+        input$gillColor, input$stalkShape, input$stalkRoot, 
+        input$stalkSurfaceAboveRing, input$stalkSurfaceBelowRing, 
+        input$stalkColorAboveRing, input$stalkColorBelowRing, 
+        input$veilColor, input$ringNumber, input$ringType, 
+        input$sporePrintColor, input$population, input$habitat
+      )),
+      stringsAsFactors = FALSE
+    )
+    input <- transpose(df)
+    model <- readRDS("model/svm.RDS")
+    
+    input <- transpose(df)
+    write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+    input <- read.csv(paste("input", ".csv", sep=""), header = TRUE, stringsAsFactors = TRUE)
+    
+    levels(input$cap.shape) <- levels(msrm$cap.shape)
+    levels(input$cap.surface) <- levels(msrm$cap.surface)
+    levels(input$cap.color) <- levels(msrm$cap.color)
+    
+    levels(input$bruises) <- levels(msrm$bruises)
+    
+    levels(input$odor) <- levels(msrm$odor)
+    
+    levels(input$gill.attachment) <- levels(msrm$gill.attachment)
+    levels(input$gill.spacing) <- levels(msrm$gill.spacing) 
+    levels(input$gill.size) <- levels(msrm$gill.size) 
+    levels(input$gill.color) <- levels(msrm$gill.color)
+    
+    levels(input$stalk.shape) <- levels(msrm$stalk.shape)
+    levels(input$stalk.root) <- levels(msrm$stalk.root)
+    levels(input$stalk.surface.above.ring) <- levels(msrm$stalk.surface.above.ring)
+    levels(input$stalk.surface.below.ring) <- levels(msrm$stalk.surface.below.ring)
+    levels(input$stalk.color.above.ring) <- levels(msrm$stalk.color.above.ring)
+    levels(input$stalk.color.below.ring) <- levels(msrm$stalk.color.below.ring)
+    
+    levels(input$veil.color) <- levels(msrm$veil.color)
+    
+    levels(input$ring.number) <- levels(msrm$ring.number)
+    levels(input$ring.type) <- levels(msrm$ring.type)
+    
+    levels(input$spore.print.color) <- levels(msrm$spore.print.color)
+    
+    
+    levels(input$population) <- levels(msrm$population)
+    
+    levels(input$habitat) <- levels(msrm$habitat)    
+    
+    return(df)
+  }
+  
+  output$contents <- renderPrint({
+    if (input$btnSubmit > 0) { 
+      isolate("Calculation complete.") 
+    } else {
+      return("Server is ready for calculation.")
+    }
+  })
+  
+  output$textResult <- renderText({
+    paste(datasetInput())
+  })
+  
+  output$txtout <- renderText({
+    paste(input$capShape, input$capSurface, input$capColor, input$bruises, 
+          input$odor, input$gillAttachment, input$gillSpacing, input$gillSize, 
+          input$gillColor, input$stalkShape, input$stalkRoot, 
+          input$stalkSurfaceAboveRing, input$stalkSurfaceBelowRing, 
+          input$stalkColorAboveRing, input$stalkColorBelowRing, 
+          input$veilColor, input$ringNumber, input$ringType, 
+          input$sporePrintColor, input$population, input$habitat, sep = "\n")
+  })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
